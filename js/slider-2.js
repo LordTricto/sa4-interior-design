c031fcdc523c7e3214ecb1b3988cd0b868de255f
Olamide Idowu <53102875+LordTricto@users.noreply.github.com>
2025-01-28 20:58:20 +0100

    Update to move slider

--- js/slider-2.js
+++ js/slider-2.js
@@ -6,7 +6,12 @@ const sliderTwo = function () {
   const dotContainer = document.querySelector(".sliderTwo-dots");
 
   let curSlide = 0;
+  let slideTimeout = null;
   const maxSlide = slides.length;
+
+
+
+
 
   // Functions
   const createDots = function () {
@@ -28,50 +33,33 @@ const sliderTwo = function () {
   };
 
   const goToSlide = function (slide) {
+    clearTimeout(slideTimeout);
+
+    curSlide = slide
+
     slides.forEach(
       (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
     );
-  };
 
-  // Next slide
-  const nextSlide = function () {
-    if (curSlide === maxSlide - 1) {
-      curSlide = 0;
-    } else {
-      curSlide++;
-    }
+    slideTimeout = setTimeout(() => {
+      if (curSlide === maxSlide - 1) {
+        curSlide = 0;
+      } else {
+        curSlide++;
+      }
 
-    goToSlide(curSlide);
-    activateDot(curSlide);
-  };
-
-  const prevSlide = function () {
-
-    if (curSlide === 0) {
-      curSlide = maxSlide - 1;
-    } else {
-      curSlide--;
-    }
-    goToSlide(curSlide);
-    activateDot(curSlide);
+      goToSlide(curSlide);
+      activateDot(curSlide);
+    }, 5000)
   };
 
   const init = function () {
     goToSlide(0);
     createDots();
-
     activateDot(0);
   };
+
   init();
-
-  // Event handlers
-  // btnRight.addEventListener("click", nextSlide);
-  // btnLeft.addEventListener("click", prevSlide);
-
-  document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowLeft") prevSlide();
-    e.key === "ArrowRight" && nextSlide();
-  });
 
   dotContainer.addEventListener("click", function (e) {
     if (e.target.classList.contains("sliderTwo-dots__dot")) {
