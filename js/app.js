53a52095bf8b7bab861f56706252e22c4cb7a78a
Olamide Idowu <53102875+LordTricto@users.noreply.github.com>
2025-01-29 20:55:11 +0100

    animation for index page

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ const categoryTabs = document.querySelector(".category__tab-container");
 const tabs = document.querySelectorAll(".category__tab");
 const tabsContent = document.querySelectorAll(".category__content");
 
+const hiddenElements = document.querySelectorAll(".hidden");
+
 let counter = 1;
 
 function fillStep() {
@@ -20,7 +22,7 @@ function fillStep() {
 }
 setTimeout(fillStep, 800);
 
-categoryTabs.addEventListener("click", function (e) {
+categoryTabs?.addEventListener("click", function (e) {
   const clicked = e.target.closest(".category__tab");
 
   // Guard clause
@@ -38,4 +40,56 @@ categoryTabs.addEventListener("click", function (e) {
     .querySelector(`.category__content--${clicked.dataset.tab}`)
     .classList.add("active-category-content");
 });
+
+// use a script tag or an external JS file
+// document.addEventListener("DOMContentLoaded", () => {
+//   gsap.registerPlugin(ScrollTrigger)
+//   let trigger = gsap.utils.toArray('.hidden');
+
+
+
+//   trigger.forEach(element => {
+//     const q = gsap.utils.selector(element);
+//     // gsap code here!
+
+
+//     const tl = gsap.timeline({
+//       scrollTrigger: {
+//         trigger: element,
+//         start: 'top center',
+//         end: 'bottom top',
+//         markers: true,
+//       }
+//     });
+
+//     tl.to(q, {
+//       scale: 2,
+
+//       // scale: 1.5
+//     });
+
+
+//     // console.log(q, element)
+//     // gsap.from(q, {
+//     //   translateY: "10%",
+//     //   duration: 2,
+//     //   scrollTrigger: {
+//     //     trigger: element,
+//     //     markers: true
+//     //   }
+//     // })
+
+//   })
+// });
+
+// const observer = new IntersectionObserver((entries) =>
+//   entries.forEach((entry) => {
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add('show');
+//     }
+//   }));
+
+// const hiddenElements = document.querySelectorAll(".hidden");
+// hiddenElements.forEach((el) => observer.observe(el));
+
 
